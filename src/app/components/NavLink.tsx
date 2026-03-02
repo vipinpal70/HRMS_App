dff9383006ae2d955c8163885d297edbecf83548
vipin pal <vipinpal7060@gmail.com>
2026-03-02 10:28:24 +0530

    feat: add NavLink component with active state highlighting

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { forwardRef } from "react";
-import { cn } from "../lib/utils";
+import { cn } from "@/lib/utils";
 
 interface NavLinkCompatProps {
   href: string;
